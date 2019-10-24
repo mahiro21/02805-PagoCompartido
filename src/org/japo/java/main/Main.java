5d7bdb4567c6f48c42849cce6c102f2b3502a0dd
mahiro21 <55140094+mahiro21@users.noreply.github.com>
2019-10-24 16:24:59 +0200

    :zap: fix license

--- src/org/japo/java/main/Main.java
+++ src/org/japo/java/main/Main.java
@@ -1,5 +1,5 @@
-//* 
-/* Copyright 2019 Adrian Bueno Olmedo <adrian.bueno.alum@iescamp.es>.
+/* 
+ * Copyright 2019 Mario Merlos Abella <mario.merlos.alum@iescamp.es>.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -29,17 +29,17 @@ public class Main {
                     .useLocale(Locale.ENGLISH).useDelimiter("\\s+");
 
     public static void main(String[] args) {
-        double Importe, Comensales, ImportePersona;
+        double importe, comensales, importePersona;
 
         try {
             // Incorporación datos  
             System.out.printf("Importe cena (€) ..: ");
-            Importe = SCN.nextInt();
+            importe = SCN.nextInt();
             System.out.printf("Número comensales .: ");
-            Comensales = SCN.nextInt();
-            ImportePersona = Importe / Comensales;
+            comensales = SCN.nextInt();
+            importePersona = importe / comensales;
             //Mensaje
-            System.out.printf("Pago por comensal .: %.2f%n", ImportePersona);
+            System.out.printf("Pago por comensal .: %.2f%n", importePersona);
         } catch (Exception e) {
             //Mensaje
             System.out.printf("ERROR: Entrada incorrecta");
